7b5b6fffa1e45a3f0bd8a65d1756568f413edb8b
Matthew Gordon <inverarity@gmail.com>
2017-11-11 15:29:46 -0800

    Map headers

--- fbiucr2016/transform_shr.py
+++ fbiucr2016/transform_shr.py
@@ -3,12 +3,25 @@ import sys
 
 DATA_LINE_REGEX = re.compile('^[\s\d]\s*\d\d')  # lines containing full or partial data match this
 PARTIAL_LINE_REGEX = re.compile('^\s{37}\s*\d*')
+HEADERS = "AGENCY,G,MO,HOM,INC_NUM,SIT,VA,VS,VR,VE,OA,OS,OR,OE,WEAP,REL,CIR,SUB,AGENCY NAME,STATE" \
+    .split(",")
+
+
+def is_victim(line):
+    line.startswith(' ' * 37)
+
+
+def is_offender(line):
+    line.startswith(' ' * 51)
 
 
 def main():
     for line in sys.stdin:
         if DATA_LINE_REGEX.search(line):
-            if PARTIAL_LINE_REGEX.search(line):
+            if not PARTIAL_LINE_REGEX.search(line):
+                last_full_line = line
+                fields = line.split("\s*")
+
                 print("-> " + line.strip())
             else:
                 print(line.strip())
