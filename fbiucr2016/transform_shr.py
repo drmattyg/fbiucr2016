bb3bee6e17cfbd883350528524648158c8f2ec3b
Matthew Gordon <inverarity@gmail.com>
2017-11-11 15:18:08 -0800

    Full and partial data line regexes

--- fbiucr2016/transform_shr.py
+++ fbiucr2016/transform_shr.py
@@ -1,9 +1,17 @@
+import re
 import sys
+
+DATA_LINE_REGEX = re.compile('^[\s\d]\s*\d\d')  # lines containing full or partial data match this
+PARTIAL_LINE_REGEX = re.compile('^\s{37}\s*\d*')
 
 
 def main():
     for line in sys.stdin:
-        print(line)
+        if DATA_LINE_REGEX.search(line):
+            if PARTIAL_LINE_REGEX.search(line):
+                print("-> " + line.strip())
+            else:
+                print(line.strip())
 
 
 if __name__ == '__main__':
