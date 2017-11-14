774ff37507f0d6e2c5bf713418f8ed35f5e69e9f
Matthew Gordon <inverarity@gmail.com>
2017-11-13 20:28:12 -0800

    Making this easier to run

--- fbiucr2016/transform_shr.py
+++ fbiucr2016/transform_shr.py
@@ -3,7 +3,9 @@ import sys
 
 DATA_LINE_REGEX = re.compile('^[\s\d]\s*\d\d')  # lines containing full or partial data match this
 PARTIAL_LINE_REGEX = re.compile('^\s{37}\s*\d*')
-HEADERS = "AGENCY,G,MO,HOM,INC_NUM,SIT,VA,VS,VR,VE,OA,OS,OR,OE,WEAP,REL,CIR,SUB,AGENCY NAME,STATE" \
+HEADERS = "AGENCY,G,MO,HOM,INC_NUM,SIT,VA,VS,VR,VE,OA,OS,OR,OE,WEAP,REL,CIR,=]" \
+          "/////////////" \
+          "////////SUB,AGENCY NAME,STATE" \
     .split(",")
 
 
@@ -16,7 +18,15 @@ def is_offender(line):
 
 
 def main():
-    for line in sys.stdin:
+    process_file(sys.stdin, sys.stdout)
+
+
+def process_file(input_file='data/2016 SHR FIle LIst.txt', output_file=sys.stdout):
+    if type(input_file) is str:
+        input_file = open(input_file, "r")
+    if type(output_file) is str:
+        output_file = open(output_file, "w")
+    for line in input_file.readlines():
         if DATA_LINE_REGEX.search(line):
             if not PARTIAL_LINE_REGEX.search(line):
                 last_full_line = line
@@ -24,7 +34,7 @@ def main():
 
                 print("-> " + line.strip())
             else:
-                print(line.strip())
+                print(line.strip(), file=output_file)
 
 
 if __name__ == '__main__':
