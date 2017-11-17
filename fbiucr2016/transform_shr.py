bcc07a0334d82f60ce4d0a3242173387e422e416
Matthew Gordon <inverarity@gmail.com>
2017-11-17 08:19:46 -0800

    Adding fixed header field sizes, need to check these

--- fbiucr2016/transform_shr.py
+++ fbiucr2016/transform_shr.py
@@ -5,14 +5,16 @@ DATA_LINE_REGEX = re.compile('^[\s\d]\s*\d\d')  # lines containing full or parti
 PARTIAL_LINE_REGEX = re.compile('^\s{37}\s*\d*')
 HEADERS = "AGENCY,G,MO,HOM,INC_NUM,SIT,VA,VS,VR,VE,OA,OS,OR,OE,WEAP,REL,CIR,SUB,AGENCY NAME,STATE" \
     .split(",")
+FIELD_OFFSETS = [5, 16, 18, 22, 27, 33, 38, 42, 45, 48, 52, 56, 59, 62, 66, 72, 77, 82, 86, 112]
+assert (len(HEADERS) == len(FIELD_OFFSETS))  # need to check this before moving on
 
 
 def is_victim(line):
-    line.startswith(' ' * 37)
+    return line.startswith(' ' * 37)
 
 
 def is_offender(line):
-    line.startswith(' ' * 51)
+    return line.startswith(' ' * 51)
 
 
 def main():
@@ -25,14 +27,14 @@ def process_file(input_file='data/2016 SHR FIle LIst.txt', output_file=sys.stdou
     if type(output_file) is str:
         output_file = open(output_file, "w")
     for line in input_file.readlines():
+        fields = re.split("\s*", line.strip())
         if DATA_LINE_REGEX.search(line):
             if not PARTIAL_LINE_REGEX.search(line):
-                last_full_line = line
-                fields = line.split("\s*")
-
-                print("-> " + line.strip())
+                last_full_fields = fields
             else:
-                print(line.strip(), file=output_file)
+                if is_offender(line):
+                    pass
+                    # handle this
 
 
 if __name__ == '__main__':
